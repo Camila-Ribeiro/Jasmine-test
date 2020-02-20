32b697ae4b1a16549141a7697314581c57d80a2c
Camila <camilarf@gmail.com>
2020-02-20 16:46:43 +0000

    first commit

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,22 @@
+console.log("========================");
+console.log("test1");
+
+describe('This fizzBuzz function', function(){
+
+    beforeEach(function(){
+        number = new fizzBuzz
+    });
+    
+    describe('If the number is divisable', function() {
+        it ('should return FizzBuzz If the number is divisible by 3 and 5', function(){
+            number.divided
+        });
+    });
+    
+    
+    
+    
+    
+    
+    
+});
